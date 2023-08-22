51db62e139646e7a2f36bcb48f7f0ef36d471901
Matheus Torres <matheustorres1201@gmail.com>
2023-08-22 13:26:26 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+
+document.getElementById('fetchColorsButton').addEventListener('click', async () => {
+    clearErrorMessage();
+    const url = document.getElementById('urlInput').value;
+    const fetchButton = document.getElementById('fetchColorsButton');
+    const loadingIndicator = document.getElementById('loadingIndicator');
+
+    //mostra animação de loading
+    fetchButton.style.display = 'none';
+    loadingIndicator.style.display = 'block';
+
+    const response = await fetch('https://back-end-one-tau.vercel.app/scrape', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url })
+    });
+
+    //Esconde animação de loading
+    loadingIndicator.style.display = 'none';
+    fetchButton.style.display = 'block';
+
+    if (response.ok) {
+        const data = await response.json();
+        const colorsContainer = document.getElementById('colorsContainer');
+        colorsContainer.innerHTML = '';
+        clearErrorMessage();
+
+        data.colors.forEach(color => {
+            const colorDiv = document.createElement('div');
+            colorDiv.style.backgroundColor = color;
+            colorDiv.className = 'color-box';
+            colorDiv.addEventListener('click', () => copyColorToClipboard(color));
+            colorsContainer.appendChild(colorDiv);
+        });
+    } else {
+        const errorMessage = document.getElementById('errorMessage');
+        errorMessage.textContent = 'Erro ao obter as cores. Verifique a URL e tente novamente.';
+    }
+});
+
+function clearErrorMessage() {
+    const errorMessage = document.getElementById('errorMessage');
+    errorMessage.textContent = '';
+}
+
+function rgbToHex(rgb) {
+    const values = rgb.match(/\d+/g);
+    return '#' + values.map(value => Number(value).toString(16).padStart(2, '0')).join('');
+}
+
+function copyColorToClipboard(color) {
+    const copyNotification = document.getElementById('copyNotification');
+    const copyText = document.createElement('textarea');
+    const hexColor = rgbToHex(color);
+    copyText.value = hexColor;
+    document.body.appendChild(copyText);
+    copyText.select();
+    document.execCommand('copy');
+    document.body.removeChild(copyText);
+
+    copyNotification.style.display = 'block';
+    setTimeout(() => {
+        copyNotification.style.display = 'none';
+    }, 2000); // Exibe a notificação por 2 segundos
+}
+
+// Oculta a notificação ao carregar a página
+window.onload = () => {
+    const copyNotification = document.getElementById('copyNotification');
+    copyNotification.style.display = 'none';
+};
